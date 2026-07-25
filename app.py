7fa85655dc112c3c5d907e937d8eedcc58537a15
sumankishore7904-bit <sumankishore7904@gmail.com>
2026-07-26 01:34:42 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
 import streamlit as st
-
-st.set_page_config(page_title="Agentic AI Study Assistant", page_icon="📚")
-st.title("📚 Agentic AI Study Assistant")
-
-st.sidebar.header("Navigation")
-st.write("Welcome! Upload your study material or ask a question to get started )
+st.title("Agentic AI Study Assistant")
+st.write("Welcome!")
